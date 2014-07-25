bc6d1f2122d81fe945e39b6b28976724cb0a5e83
Charlie LaMothe <charlie@keypr.com>
2014-07-25 16:20:51 -0700

    Address and PhoneNumber from the OXI XSDs are now deserialized into Strings instead of Enums, as OXI sometimes sends values not found in the XSD's enum declaration. (Per Costin's suggestion)

---
+++ MicrosIds/src/main/java/com/keypr/pms/micros/oxi/ids/MicrosIds.java
@@ -0,0 +1,47 @@
+package com.keypr.pms.micros.oxi.ids;
+
+/**
+ * Ids and constants used in Micros
+ *
+ * @author Charlie La Mothe (charlie@keypr.com)
+ */
+public class MicrosIds {
+
+    public static class OXI {
+        /**
+         * Refers to //xs:simpleType [@name='phoneNumberType'] of profile.fidelio.3.0.xsd and profile.fidelio.4.0.xsd
+         *
+         * Although the XSDs specify that it's value is restricted to the following enum values,
+         * OXI sometimes returns other values.
+
+         * To handle this case, this type is deserialized to a String rather than an enum.
+         */
+        public enum PhoneNumberType {
+            OTHER,
+            HOME,
+            BUSINESS,
+            MOBILE,
+            HOMEFAX,
+            BUSINESSFAX,
+            PAGER,
+            TELEX,
+            TTY
+        }
+
+        /**
+         * Refers to //xs:simpleType [@name='addressType'] of profile.fidelio.3.0.xsd and profile.fidelio.4.0.xsd
+         *
+         * Although the XSDs specify that it's value is restricted to the following enum values,
+         * OXI sometimes returns other values.
+         *
+         * To handle this case, this type is deserialized to a String rather than an enum.
+         */
+        public enum AddressType {
+            HOME,
+            BUSINESS,
+            MAILING,
+            SHIPPING,
+            BILLING
+        }
+    }
+}
