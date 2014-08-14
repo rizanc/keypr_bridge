c4051c0497343438b5203227297dc741c57d7232
Charlie La Mothe <charlie@keypr.com>
2014-08-14 14:51:32 -0700

    Disabled the REST endpoint for MeetingRoomInformationRequest, which has not been implemented,

--- CloudKeyHTWebServices/src/main/java/com/keypr/webservices/rest/services/HotelsResource.java
+++ CloudKeyHTWebServices/src/main/java/com/keypr/webservices/rest/services/HotelsResource.java
@@ -39,24 +39,25 @@ public class HotelsResource extends AbstractResource {
         return messageParser.hotelInformationQuery(request);
     }
 
-    @Path("/meetingrooms")
-    @GET
-    @ApiOperation(
-        value = "Fetches a list of available meeting rooms which match some criteria",
-        response = MeetingRoomInformationResponse.class
-    )
-    @ApiResponses({
-	    @ApiResponse(code = 422, message = "Request parameters are incomplete or invalid"),
-	    @ApiResponse(code = 400, message = "The PMS responded with an error message"),
-        @ApiResponse(code = 502, message = "An unexpected error occurred involving PMS communication")
-    })
-    public MeetingRoomInformationResponse meetingRoomInformation(
-            @QueryParam("numberOfAttendees") Integer numberOfAttendees) {
-        MeetingRoomInformationRequest request = new MeetingRoomInformationRequest(numberOfAttendees);
-        validate(request);
-
-	    MeetingRoomInformationResponse meetingInformation = messageParser.getMeetingInformation(request);
-	    return meetingInformation;
-    }
+	// TODO: Complete the implementation of this and re-enable the endpoint
+//    @Path("/meetingrooms")
+//    @GET
+//    @ApiOperation(
+//        value = "Fetches a list of available meeting rooms which match some criteria",
+//        response = MeetingRoomInformationResponse.class
+//    )
+//    @ApiResponses({
+//	    @ApiResponse(code = 422, message = "Request parameters are incomplete or invalid"),
+//	    @ApiResponse(code = 400, message = "The PMS responded with an error message"),
+//        @ApiResponse(code = 502, message = "An unexpected error occurred involving PMS communication")
+//    })
+//    public MeetingRoomInformationResponse meetingRoomInformation(
+//            @QueryParam("numberOfAttendees") Integer numberOfAttendees) {
+//        MeetingRoomInformationRequest request = new MeetingRoomInformationRequest(numberOfAttendees);
+//        validate(request);
+//
+//	    MeetingRoomInformationResponse meetingInformation = messageParser.getMeetingInformation(request);
+//	    return meetingInformation;
+//    }
 
 }
