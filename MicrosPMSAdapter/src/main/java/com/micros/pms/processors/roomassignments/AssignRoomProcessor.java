5774241c73aa2ceaafb6af13bb7e1ec2a4f81587
Charlie La Mothe <charlie@keypr.com>
2014-08-29 09:58:29 -0700

    Minor unused imports cleanup

--- MicrosPMSAdapter/src/main/java/com/micros/pms/processors/roomassignments/AssignRoomProcessor.java
+++ MicrosPMSAdapter/src/main/java/com/micros/pms/processors/roomassignments/AssignRoomProcessor.java
@@ -2,16 +2,9 @@ package com.micros.pms.processors.roomassignments;
 
 import com.cloudkey.pms.micros.og.common.ResultStatus;
 import com.cloudkey.pms.micros.og.core.OGHeader;
-import com.cloudkey.pms.micros.og.hotelcommon.HotelReference;
-import com.cloudkey.pms.micros.og.hotelcommon.RoomStatus;
-import com.cloudkey.pms.micros.og.reservation.advanced.FetchRoomStatusRequest;
-import com.cloudkey.pms.micros.og.reservation.advanced.FetchRoomStatusResponse;
 import com.cloudkey.pms.micros.services.ReservationServiceSoap;
-import com.cloudkey.pms.micros.services.ResvAdvancedServiceSoap;
 import com.cloudkey.pms.request.roomassignments.AssignRoomRequest;
 import com.cloudkey.pms.response.roomassignments.AssignRoomResponse;
-import com.google.common.base.Optional;
-import com.google.common.collect.FluentIterable;
 import com.google.inject.Inject;
 import com.micros.pms.processors.OWSProcessor;
 
