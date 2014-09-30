8d2f0acfca670f57347d017a4f62c8c49815c22d
Charlie La Mothe <charlie@keypr.com>
2014-09-30 12:12:26 -0700

    Disabled CloudKeyHTDatabase, which is not used by any modules.

--- MicrosDataHarvester/src/main/java/com/micros/harvester/DataHarvesterModule.java
+++ MicrosDataHarvester/src/main/java/com/micros/harvester/DataHarvesterModule.java
@@ -1,6 +1,5 @@
 package com.micros.harvester;
 
-import com.cloudkey.db.DatabaseModule;
 import com.cloudkey.util.GuiceUtils;
 import com.google.inject.AbstractModule;
 import com.micros.pms.MicrosPMSModule;
@@ -13,7 +12,6 @@ public class DataHarvesterModule extends AbstractModule {
 	protected void configure() {
 		install(new MicrosPMSModule());
 		install(new com.keypr.jackson.KeyprJacksonModule());
-		install(new DatabaseModule());
 
 		GuiceUtils.bindProperties(binder(), getClass().getResourceAsStream("dataharvester.properties"));
 	}
