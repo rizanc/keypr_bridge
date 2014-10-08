0ea28495822cbc8a28957d0dc6dfc5536c322c7b
Charlie La Mothe <charlie@clamothe.com>
2014-10-08 11:04:02 -0700

    Added command-line tool for generating JsonSchemas for a class.

--- CloudKeyHTJsonSchemaGenerator/src/main/java/com/keypr/jsonschema/JsonSchemaGeneratorApplication.java
+++ CloudKeyHTJsonSchemaGenerator/src/main/java/com/keypr/jsonschema/JsonSchemaGeneratorApplication.java
@@ -1,6 +1,5 @@
 package com.keypr.jsonschema;
 
-import com.cloudkey.pms.common.reservation.Reservation;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.google.inject.Guice;
 import com.google.inject.Injector;
@@ -10,13 +9,15 @@ import com.google.inject.Injector;
  */
 public class JsonSchemaGeneratorApplication {
 
-	public static void main(String[] args) throws JsonProcessingException {
+	public static void main(String[] args) throws JsonProcessingException, ClassNotFoundException {
+		Class<?> aClass = Class.forName(args[0]);
+
 		Injector injector = Guice.createInjector(new JsonSchemaGeneratorModule());
 
 		JsonSchemaGenerator generator = injector.getInstance(JsonSchemaGenerator.class);
 
-		// Prints an auto-generated json schema for the Reservation class
-		System.out.println(generator.serializeSchema(generator.generateFor(Reservation.class)));
+		// Prints an auto-generated json schema for the given class
+		System.out.println(generator.serializeSchema(generator.generateFor(aClass)));
 	}
 }
 
